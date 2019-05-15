39514f5fa624cf35428b172aa1f345a402b2a3f0
RajasekharwhY <yakkali.rajasekhar@gmail.com>
2019-05-14 22:46:20 -0400

    serving a string

---
+++ 30daysOfNode/serve_a_string_in_response.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+var host = "127.0.0.1"
+var port = 3000
+
+var server = http.createServer((req, res)=>{
+res.writeHead(200, {"content-Type": "text/plain"});
+res.write("Hellow World ! \n");
+res.end("server working");
+});
+
+server.listen(port,host, (error) => { 
+if(error)
+{ 
+  return console.log("Error occured :", error);
+}
+console.log(`server is listening on host ${host} and port ${port}`);
+});
