c6794de07010b924ab03728a585fb53f1f4cd22c
RajasekharwhY <yakkali.rajasekhar@gmail.com>
2019-05-14 22:17:35 -0400

    create server using node.

---
+++ 30daysOfNode/createServer.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+var host = "120.0.0.1"
+var port = 3000
+
+var server = createServer((req, res)=>{
+res.writeHead(200, {"content-Type": "text/plain"});
+console.log("server Working");
+res.end("server working");
+});
+
+server.listen(port,host, (error) => { 
+if(error)
+{ 
+  return console.log("Error occured :", error);
+}
+console.log('server is listening on ${port} and port ${host}');
+});
